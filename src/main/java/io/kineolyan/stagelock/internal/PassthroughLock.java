61728e78b9527f9be53aa357ad06fe9265059504
Kineolyan <kineolyan@protonmail.com>
2023-02-26 10:16:16 +0100

    Revert "Attempt with counter and sentinel, but not working."
    This reverts commit 6023cab0b86c4c360adaa4a33cdcf91290a368f3.

--- src/main/java/io/kineolyan/stagelock/internal/PassthroughLock.java
+++ src/main/java/io/kineolyan/stagelock/internal/PassthroughLock.java
@@ -15,105 +15,42 @@ public final class PassthroughLock implements Lock {
     private final AtomicInteger counter = new AtomicInteger(0);
     private final GateTaker lockStage;
     private final Runnable unlockStage;
-    private final Object signal = new Object();
 
 
     @Override
     public void lock() {
-        while (true) {
-            val optimisticStart = incrementCounter();
-            if (optimisticStart < 0) {
-                waitSignal(null);
-            } else {
-                break;
-            }
-        }
         this.lockStage.acquire();
+        incrementCounter();
     }
 
-    private int incrementCounter() {
-        return this.counter.updateAndGet(count -> {
-            if (count < 0) {
-                return count;
-            } else {
-                return count + 1;
-            }
-        });
-    }
-
-    private void waitSignal(final Duration timeout) {
-        synchronized (this.signal) {
-            try {
-                if (timeout == null) {
-                    this.signal.wait();
-                } else {
-                    this.signal.wait(timeout.toMillisPart(), timeout.toNanosPart());
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private void waitSignalInterruptibly(final Duration timeout) throws InterruptedException {
-        synchronized (this.signal) {
-            if (timeout == null) {
-                this.signal.wait();
-            } else {
-                this.signal.wait(timeout.toMillisPart(), timeout.toNanosPart());
-            }
-        }
+    private void incrementCounter() {
+        this.counter.incrementAndGet();
     }
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
-        while (true) {
-            val optimisticStart = incrementCounter();
-            if (optimisticStart < 0) {
-                waitSignalInterruptibly(null);
-            } else {
-                break;
-            }
-        }
-        this.lockStage.acquire();
+        this.lockStage.acquireInterruptibly();
+        incrementCounter();
     }
 
     @Override
     public boolean tryLock() {
-        while (true) {
-            val optimisticStart = incrementCounter();
-            if (optimisticStart < 0) {
-                waitSignal(null);
-            } else {
-                break;
-            }
-        }
         val acquired = this.lockStage.tryAcquire(Duration.ZERO);
         if (acquired) {
+            incrementCounter();
             return true;
         } else {
-            // Still not good, we can go from a false 1 -> 0, but we don't need to release the gate
-            this.counter.decrementAndGet();
             return false;
         }
     }
 
     @Override
     public boolean tryLock(final long time, final TimeUnit unit) throws InterruptedException {
-        while (true) {
-            val optimisticStart = incrementCounter();
-            if (optimisticStart < 0) {
-                waitSignal(Duration.of(time, unit.toChronoUnit()));
-            } else {
-                break;
-            }
-        }
         val acquired = this.lockStage.tryAcquireInterruptibly(Duration.of(time, unit.toChronoUnit()));
         if (acquired) {
+            incrementCounter();
             return true;
         } else {
-            // Still not good, we can go from a false 1 -> 0, but we don't need to release the gate
-            this.counter.decrementAndGet();
             return false;
         }
     }
@@ -136,10 +73,7 @@ public final class PassthroughLock implements Lock {
         if (remaining == -1) {
             this.unlockStage.run();
             val reset = this.counter.compareAndSet(-1, 0);
-            assert reset : "Failed to reset the lock";
-            synchronized (this.signal) {
-                this.signal.notifyAll();
-            }
+            assert reset: "Failed to reset the lock";
         }
     }
 
